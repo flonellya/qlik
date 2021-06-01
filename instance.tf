84d04835c4a8a4f3fc0527627f1cbbc071db3975
flonellya <flonellya@gmail.com>
2021-06-01 18:17:42 -0400

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-gov-west-1"
+}
+
+resource "aws_instance" "qlik_replicate" {
+  ami           = "ami-0bbf3595bb2fb39ec"
+  instance_type = "t3.medium"
+  subnet_id =  "subnet-0ab643787de239342"
+  key_name = "qlik_west"
+
+  tags = {
+    Name = "Attunity-replicate"
+    Auto-StartStop-Enabled = "true"
+    Environment = "Test"
+    Poc = "florence.vinyo@manteh.com"
+  }
+}
+
