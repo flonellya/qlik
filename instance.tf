159ecd3e5203726cf4d9d588459e0c030e73b478
flonellya <flonellya@gmail.com>
2021-06-22 15:00:22 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -12,38 +12,6 @@ provider "aws" {
   region  = "us-gov-west-1"
 }
 
-resource "aws_instance" "qlik_replicate" {
-  ami           = "ami-0bbf3595bb2fb39ec"
-  instance_type = "t3.medium"
-  subnet_id =  "subnet-0ab643787de239342"
-  key_name = "qlik_west"
-  security_groups = ["sg-039f2d81b207cb7da"]
-
-  tags = {
-    Name = "Attunity-replicate"
-    Auto-StartStop-Enabled = "true"
-    Environment = "Test"
-    Poc = "florence.vinyo@manteh.com"
-  }
-}
-output "instance_ip_addr" {
-  value = aws_instance.qlik_replicate.private_ip
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-gov-west-1"
-}
-
 resource "aws_instance" "nprinting" {
   ami           = "ami-0de2cf70c88e410e4"
   instance_type = "t3.medium"
